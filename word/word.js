f5837fbfc8f4cf2ec31a361df4484d41fbe7f588
yyyujinnn <yujink129129@gmail.com>
2023-06-24 01:23:16 +0900

    word random

--- word/word.js
+++ word/word.js
@@ -1,26 +1,32 @@
-// 게임 시작 전 카운트다운 3,2,1,start
-
-// let stringArr = ["3","2","1","START"];
-// let selectString = stringArr[Math.random() * stringArr.length];
-
-let stringArr = ["죽 마 O O", "고 등 O O", "아 프 O O", "스 나 O O"];
-
 function start()
 {   
-    let selectString = stringArr[Math.random() * stringArr.length];
+    // 네글자 단어 랜덤 추출
+    var words = ["죽 마 O O", "고 등 O O", "아 프 O O", "스 나 O O"];
 
-    var count = document.getElementById('sec');
-    var html;
+    var word = document.getElementById("word");
+    var random_words = Math.floor(Math.random() * words.length); // 랜덤 정수 추출
+
+    word.innerHTML = words[random_words];
+
+    // var sec = 5;
+    // var counter = setInterval(timer, 1000);
+    // sec--;
+
+    // if(sec <= 0)
+
+    
+    // var count = document.getElementById('sec');
+    // var html;
    
-    for(count = 5; count > 0; count--)
-    {
-         count--;
-    }
+    // for(count = 5; count > 0; count--)
+    // {
+    //      count--;
+    // }
 
-    if(count <= 0)
-    {
-        html = '<div>종료</div>';
-    }
+    // if(count <= 0)
+    // {
+    //     html = '<div>종료</div>';
+    // }
 
-    count.innerHTML = html;
+    // count.innerHTML = html;
 }
