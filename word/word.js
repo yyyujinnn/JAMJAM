c3965d575dc1bcf85b3295fd9f434dbfdf3e077b
yyyujinnn <yujink129129@gmail.com>
2023-06-24 01:37:44 +0900

    wordlist

--- word/word.js
+++ word/word.js
@@ -1,7 +1,11 @@
 function start()
 {   
     // 네글자 단어 랜덤 추출
-    var words = ["죽 마 O O", "고 등 O O", "아 프 O O", "스 나 O O"];
+    var words = ["죽 마 O O", "고 등 O O", "아 프 O O", "스 나 O O", 
+    "도 라 O O", "딸 기 O O", "모 래 O O", "바 람 O O", "블 루 O O", 
+    "아 이 O O", "중 간 O O", "기 말 O O", "인 피 O O", "데 이 O O", 
+    "초 코 O O", "사 제 O O", "신 서 O O", "카 푸 O O", "포 스 O O",
+    "양 념 O O", "전 신 O O" ];
 
     var word = document.getElementById("word");
     var random_words = Math.floor(Math.random() * words.length); // 랜덤 정수 추출
