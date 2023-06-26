84edb1e9c5beb8fae4abb294d22a00e07ed079b9
yyyujinnn <yujink129129@gmail.com>
2023-06-27 02:50:41 +0900

    word Timer

--- updown/updown.js
+++ updown/updown.js
@@ -2,39 +2,6 @@
 //let stringArr = ["3","2","1","START"];
 //let selectString = stringArr[Math.random() * stringArr.length];
 
-// random1,2,3,4 랜덤 0~9 정수 추출
-// const $random1 = document.querySelector('#random1');
-// const $random2 = document.querySelector('#random2');
-// const $random3 = document.querySelector('#random3');
-// const $random4 = document.querySelector('#random4');
-// const randomNum1 = Math.floor(Math.random() * 10);
-// const randomNum2 = Math.floor(Math.random() * 10);
-// const randomNum3 = Math.floor(Math.random() * 10);
-// const randomNum4 = Math.floor(Math.random() * 10);
-
-// $random1.append(randomNum1, document.createElement('br'));
-// $random2.append(randomNum2, document.createElement('br'));
-// $random3.append(randomNum3, document.createElement('br'));
-// $random4.append(randomNum4, document.createElement('br'));
-
-// function game() {
-//     var num1 = document.getElementById("random1").value;
-//     if (num1 == "") {
-//         document.getElementById("random1").style.backgroundColor="red";
-//     }
-//     else if (num1 == blockNum1) {
-//         document.getElementById("random1").style.backgroundColor="green";
-//     }
-//     else {
-//         if (ran > num1) {
-//             // 위 화살표 이미지 
-// 			<input type="file" src></input>
-//         }
-//         else {
-//             // 아래화살표
-//         }
-//     }
-// }
 
 $(document).ready(function() {
 // 게임시작 버튼 클릭 이벤트
@@ -77,11 +44,3 @@ else if (Number(randomValue) < Number(txtAnswer)) {
     $("#gameArea").append("<p>정답은 " + txtAnswer + "보다 " + "<span style = color:blue>작은 수 </span>" + "입니다.</p>");  
 }
 }
-
-// focus가 다음 input 박스로 자동 이동
-// $("#random1").on("keyup",function(){
-    
-// 	if(this.value.length == 1){
-// 		$("#random2").focus();
-// 	}	
-// });
--- word/word.js
+++ word/word.js
@@ -1,36 +1,43 @@
 function start()
 {   
+    // 3초 타이머
+    var time = 3;
+    var sec="";
+      
+    //setInterval(함수, 시간) : 주기적인 실행
+    var timer = setInterval( function() {
+        sec = time;
+        
+        document.getElementById("sec").innerHTML = sec;
+        time--;
+        
+        //타임아웃 
+        if (time < 0) {
+            clearInterval(timer); // setInterval() 실행 끝
+            // 다시 랜덤 추출
+        }
+    }, 700 );
+    
     // 네글자 단어 랜덤 추출
     var words = ["죽 마 O O", "고 등 O O", "아 프 O O", "스 나 O O", 
     "도 라 O O", "딸 기 O O", "모 래 O O", "바 람 O O", "블 루 O O", 
     "아 이 O O", "중 간 O O", "기 말 O O", "인 피 O O", "데 이 O O", 
     "초 코 O O", "사 제 O O", "신 서 O O", "카 푸 O O", "포 스 O O",
-    "양 념 O O", "전 신 O O" ];
+    "양 념 O O", "전 신 O O", "비 트 O O", "방 방 O O", "도 원 O O",
+    "스 파 O O", "대 한 O O", "누 네 O O", "어 장 O O", "버 터 O O",
+    "업 데 O O", "카 페 O O", "붉 은 O O", "소 녀 O O", "스 타 O O",
+    "백 설 O O", "요 구 O O", "파 인 O O", "오 토 O O", "사 자 O O",
+    "비 밀 O O", "계 좌 O O", "우 리 O O", "생 년 O O", "알 레 O O",
+    "종 이 O O", "주 의 O O", "삼 각 O O", "미 끄 O O", "호 랑 O O",
+    "뭉 게 O O", "샌 드 O O", "탄 수 O O", "하 모 O O", "신 용 O O",
+    "하 드 O O", "아 카 O O", "롯 데 O O", "플 라 O O", "맘 스 O O",
+    "허 리 O O", "인 어 O O", "마 요 O O", "오 버 O O", "겨 울 O O"];
 
     var word = document.getElementById("word");
     var random_words = Math.floor(Math.random() * words.length); // 랜덤 정수 추출
-
+        
     word.innerHTML = words[random_words];
-
-    // var sec = 5;
-    // var counter = setInterval(timer, 1000);
-    // sec--;
-
-    // if(sec <= 0)
-
     
-    // var count = document.getElementById('sec');
-    // var html;
-   
-    // for(count = 5; count > 0; count--)
-    // {
-    //      count--;
-    // }
+    document.getElementById("start").innerHTML = "다음문제";
 
-    // if(count <= 0)
-    // {
-    //     html = '<div>종료</div>';
-    // }
-
-    // count.innerHTML = html;
 }
