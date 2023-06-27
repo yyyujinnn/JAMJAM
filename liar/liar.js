a8f711b10757edbcad5ccd9b90e7f4cf8643b7a1
yyyujinnn <yujink129129@gmail.com>
2023-06-28 01:08:36 +0900

    count

---
+++ liar/liar.js
@@ -0,0 +1,39 @@
+//인원수 4명일때 일반3 라이어1
+function ver4(){
+    document.getElementById("count").innerHTML = "라이어는 1명입니다.";
+
+    //버튼은 hide()
+}
+
+
+
+//인원수 5명일때 일반4 라이어1
+function ver5(){
+    document.getElementById("count").innerHTML = "라이어는 1명입니다.";
+}
+
+//인원수 6명일때 일반4 라이어2
+function ver6(){
+    document.getElementById("count").innerHTML = "라이어는 2명입니다.";
+}
+
+//인원수 7명일때 일반5 라이어2
+function ver7(){
+    document.getElementById("count").innerHTML = "라이어는 2명입니다.";
+}
+
+//인원수 8명일때 일반6 라이어2
+function ver8(){
+    document.getElementById("count").innerHTML = "라이어는 2명입니다.";
+}
+
+//인원수 9명일때 일반6 라이어3
+function ver9(){
+    document.getElementById("count").innerHTML = "라이어는 3명입니다.";
+}
+
+//인원수 10명일때 일반7 라이어3
+function ver10(){
+    document.getElementById("count").innerHTML = "라이어는 3명입니다.";
+}
+
